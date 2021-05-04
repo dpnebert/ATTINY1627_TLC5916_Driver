1a485d5f7cad49bc761b128d886951d5b2fd074e
dpnebert <dpnebert@gmail.com>
2021-05-03 21:09:34 -0500

    Add files via upload

--- ATTINY1627_TLC5916_FULL/ATTINY1627_TLC5916_FULL/include/TLC5916.h
+++ ATTINY1627_TLC5916_FULL/ATTINY1627_TLC5916_FULL/include/TLC5916.h
@@ -5,14 +5,40 @@
  *  Author: dpneb
  */ 
 
+#include "include/atmel_start_pins.h"
 #include "TLC5916_Util.h"
 
 #ifndef TLC5916_H_
 #define TLC5916_H_
 
-void transmitCharacter(char p);
+#define ALL_ON			255
+#define ALL_OFF			0
 
+#define STSM_OE			29
+#define STSM_LE			8
 
+#define STNM_OE			29
+#define STNM_LE			0
+
+#define WCC_OE			255
+#define WCC_LE			128
+
+#define DATA_NULL		0
+
+#define PACKET_FULL		8
+#define PACKET_MODE		5
+
+void transmitCharacter(char p, char c);
+void enableOutput();
+void disableOutput();
+void allOn();
+void allOff();
+
+void switchToSpecialMode();
+void switchToNormalMode();
+
+void writeConfiguration(uint8_t c);
+uint8_t readErrorCodeStatus();
 
 
 #endif /* TLC5916_H_ */
--- ATTINY1627_TLC5916_FULL/ATTINY1627_TLC5916_FULL/include/TLC5916_Util.h
+++ ATTINY1627_TLC5916_FULL/ATTINY1627_TLC5916_FULL/include/TLC5916_Util.h
@@ -6,15 +6,17 @@
  */ 
 
 
+#include "include/atmel_start_pins.h"
 #ifndef TLC5916_UTIL_H_
 #define TLC5916_UTIL_H_
 
-void sendChar(char p);
-void latch();
-void enableOutput();
-void disableOutput();
 
-
+uint8_t readBits();
+void sendBits(char d, char o, char l, char c);
+void pulseLatch();
+void latchHigh();
+void latchLow();
+void generateErrorCodeStatus();
 
 
 #endif /* TLC5916_UTIL_H_ */
