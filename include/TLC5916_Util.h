00aa2040c65619e160c0efb3e327d4f773b09098
dpnebert <dpnebert@gmail.com>
2021-05-10 07:12:24 -0500

    Cleaned up that there main.c file
    Removed unnecessary lines and cleaned up a bit

---
+++ include/TLC5916_Util.h
@@ -0,0 +1,22 @@
+/*
+ * TLC5916_Util.h
+ *
+ * Created: 4/29/2021 10:02:47 PM
+ *  Author: dpneb
+ */ 
+
+
+#include "include/atmel_start_pins.h"
+#ifndef TLC5916_UTIL_H_
+#define TLC5916_UTIL_H_
+
+
+uint8_t readBits();
+void sendBits(char d, char o, char l, char c);
+void pulseLatch();
+void latchHigh();
+void latchLow();
+void generateErrorCodeStatus();
+
+
+#endif /* TLC5916_UTIL_H_ */
---
+++ main.c
@@ -0,0 +1,35 @@
+#include <atmel_start.h>
+#include "TLC5916.h"
+#include <util/delay.h>
+
+uint8_t outputPin;
+uint8_t latchPin;
+uint8_t outputPin;
+
+uint16_t counter;
+
+uint8_t count, brightness;
+
+void printToTerminal(uint8_t r);
+void nightRider();
+void majiger(uint8_t b);
+
+int main(void)
+{
+	brightness = 1;
+	atmel_start_init();
+	_delay_us(100000);	
+	enableOutput();
+	
+	while (1) {	
+		switchToSpecialMode();		
+		writeConfiguration(255);
+		switchToNormalMode();
+		
+		transmitCharacter(1, 8);
+		_delay_us(5000000);
+	}
+}
+
+
+
